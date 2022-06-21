452980eaee1622bc97b9b4064faf15e2871ffc09
Rodrigo Villar Valenca <rodrigovillarvalenca@gmail.com>
2022-06-21 16:55:47 -0300

    adiciona o input de texto no Search.js e adiciona as labels nos input

--- src/pages/components/Search.js
+++ src/pages/components/Search.js
@@ -3,32 +3,52 @@ import React from 'react';
 function Search() {
   return (
     <div>
-      Ingrediente:
-      <input
-        type="radio"
-        data-testid="ingredient-search-radio"
-        name="ingredient"
-        onChange={ handleRadio }
-      />
-      Nome:
-      <input
-        type="radio"
-        data-testid="name-search-radio"
-        name="name"
-        onChange={ handleRadio }
-
-      />
-      Primeira letra:
-      <input
-        type="radio"
-        data-testid="first-letter-search-radio"
-        name="first-letter"
-        onChange={ handleRadio }
-      />
+      <label htmlFor="search-input">
+        <input
+          type="text"
+          data-testid="search-input"
+          id="search-input"
+          name="search"
+          placeholder="Search"
+          value={ search }
+          onChange={ handleSearch }
+        />
+      </label>
+      <label htmlFor="ingredient-radio">
+        Ingrediente:
+        <input
+          type="radio"
+          data-testid="ingredient-search-radio"
+          name="ingredient"
+          id="ingredient-radio"
+          onChange={ handleRadio }
+        />
+      </label>
+      <label htmlFor="name-radio">
+        Nome:
+        <input
+          type="radio"
+          data-testid="name-search-radio"
+          name="name"
+          id="name-radio"
+          onChange={ handleRadio }
+        />
+      </label>
+      <label htmlFor="letter-radio">
+        Primeira letra:
+        <input
+          type="radio"
+          data-testid="first-letter-search-radio"
+          name="first-letter"
+          id="letter-radio"
+          onChange={ handleRadio }
+        />
+      </label>
       <button
         type="button"
         data-testid="exec-search-btn"
         name="search-btn"
+        id="search-btn"
       >
         Search
       </button>
