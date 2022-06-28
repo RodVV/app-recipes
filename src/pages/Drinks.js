1624b3bb250f400d322056b61741562eb40207d5
Gabriel Kauer <gjmkauer@hotmail.com>
2022-06-28 15:57:59 -0300

    Corrige merge que veio função duplicada.

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -6,26 +6,6 @@ import '../App.css';
 import DrinksRender from './components/DrinksRender';
 
 function Drinks() {
-  const dispatch = useDispatch();
-  const foodSelector = useSelector(({ foodSlice }) => foodSlice);
-  const searchSelector = useSelector(({ search }) => search);
-  const { drinks } = searchSelector.data;
-  const { initialDrinks } = foodSelector;
-  const buttonSelector = useSelector(({ buttonFilter }) => buttonFilter);
-  const { cards, allCategory } = buttonSelector;
-
-  useEffect(() => {
-    const listFiltersDrink = async () => {
-      const request = await fetch(ApiDrink);
-      const response = await request.json();
-      const filterDrink = response.drinks.filter(
-        (_drinkCategory, index) => index < TWELVE,
-      );
-      dispatch(setInitialDrinks(filterDrink));
-    };
-    listFiltersDrink();
-  }, [dispatch]);
-
   return (
     <div>
       <Header />
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -6,24 +6,6 @@ import '../App.css';
 import MealsRender from './components/MealsRender';
 
 function Foods() {
-  const dispatch = useDispatch();
-  const foodSelector = useSelector(({ foodSlice }) => (foodSlice));
-  const searchSelector = useSelector(({ search }) => (search));
-  const { meals } = searchSelector.data;
-  const { initialMeals } = foodSelector;
-  const buttonSelector = useSelector(({ buttonFilter }) => buttonFilter);
-  const { cards, allCategory } = buttonSelector;
-
-  useEffect(() => {
-    const recipesFoods = async () => {
-      const request = await fetch(ApiFoods);
-      const response = await request.json();
-      const allProducts = response.meals.filter((_food, index) => index < TWELVE);
-      dispatch(setInitialMeals(allProducts));
-    };
-    recipesFoods();
-  }, [dispatch]);
-
   return (
     <div>
       <Header />
