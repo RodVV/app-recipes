83c4023df037cc0dbc59fb9990cfc64270016d58
João M <joaomarcelofg@hotmail.com>
2022-06-29 20:41:54 -0300

    ajeitando uns detalhes

--- src/pages/components/RemoveFromFavoriteButton.js
+++ src/pages/components/RemoveFromFavoriteButton.js
@@ -2,10 +2,10 @@ import React from 'react';
 import propTypes from 'prop-types';
 import blackHeartIcon from '../../images/blackHeartIcon.svg';
 
-function RemoveFromFavoriteButton({ handleFunction, testId }) {
+function RemoveFromFavoriteButton({ handleFunction }) {
   return (
     <input
-      data-testid={ `${testId === '' ? 'favorite-btn' : testId} ` }
+      data-testid="favorite-btn"
       type="image"
       src={ blackHeartIcon }
       alt="Botão de desfavoritar"
@@ -16,10 +16,6 @@ function RemoveFromFavoriteButton({ handleFunction, testId }) {
 
 RemoveFromFavoriteButton.propTypes = {
   handleFunction: propTypes.func.isRequired,
-  testId: propTypes.string,
-};
-RemoveFromFavoriteButton.defaultProps = {
-  testId: '',
 };
 
 export default RemoveFromFavoriteButton;
